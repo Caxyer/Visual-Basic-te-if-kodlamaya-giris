28991bc26053236ae3ada42f99bf26c5bb001518
Caxyer <56657520+Caxyer@users.noreply.github.com>
2020-12-26 22:15:23 +0300

    Add files via upload

---
+++ Not If VB/Not If VB/Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(219, 37)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Gönder"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(98, 37)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(66, 15)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Not Giriniz:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(321, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 15)
+        Me.Label2.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(13, 87)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 15)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Durumu: "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.SystemColors.Control
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label4.Location = New System.Drawing.Point(77, 87)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 15)
+        Me.Label4.TabIndex = 5
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(163, 87)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(66, 15)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Harf Notu: "
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(235, 87)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(0, 15)
+        Me.Label6.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(411, 159)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ Not If VB/Not If VB/Form1.vb
@@ -0,0 +1,58 @@
+﻿Public Class Form1
+
+    Dim x As Integer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        x = Val(TextBox1.Text)
+
+        If x <= 45 And x >= 1 Then
+            Label4.Text = ("Kaldı.")
+            Label6.Text = ("FD")
+            Label2.Text = ("Kaldı. | FD")
+            MsgBox(Label4.Text + " | " + Label6.Text)
+
+        ElseIf x <= 52 And x >= 46 Then
+            Label4.Text = ("Kaldı.")
+            Label6.Text = ("DD")
+            Label2.Text = ("Kaldı. | DD")
+            MsgBox(Label4.Text + " | " + Label6.Text)
+
+        ElseIf x <= 59 And x >= 53 Then
+            Label4.Text = ("Kaldı.")
+            Label6.Text = ("DC")
+            Label2.Text = ("Kaldı. | DC")
+            MsgBox(Label4.Text + " | " + Label6.Text)
+
+        ElseIf x <= 66 And x >= 60 Then
+            Label4.Text = ("Geçti.")
+            Label6.Text = ("CC")
+            Label2.Text = ("Geçti. | CC")
+            MsgBox(Label4.Text + " | " + Label6.Text)
+
+        ElseIf x <= 73 And x >= 67 Then
+            Label4.Text = ("Geçti.")
+            Label6.Text = ("CB")
+            Label2.Text = ("Geçti. | CB")
+            MsgBox(Label4.Text + " | " + Label6.Text)
+
+        ElseIf x <= 80 And x >= 74 Then
+            Label4.Text = ("Geçti.")
+            Label6.Text = ("BB")
+            Label2.Text = ("Geçti. | BB")
+            MsgBox(Label4.Text + " | " + Label6.Text)
+
+        ElseIf x <= 87 And x >= 81 Then
+            Label4.Text = ("Geçti.")
+            Label6.Text = ("BA")
+            Label2.Text = ("Geçti. | BA")
+            MsgBox(Label4.Text + " | " + Label6.Text)
+
+        ElseIf x <= 100 And x >= 88 Then
+            Label4.Text = ("Geçti.")
+            Label6.Text = ("AA")
+            Label2.Text = ("Geçti. | AA")
+            MsgBox(Label4.Text + " | " + Label6.Text)
+
+        End If
+    End Sub
+End Class
